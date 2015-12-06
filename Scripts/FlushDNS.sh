331a3af5c65ab7dea384c0595d452d47a6190975
OwlReporter <Niresh@Nireshs-MacBook-Pro-2.local>
2015-12-06 11:54:11 +0530

    Added new line to make output beauty

--- Scripts/FlushDNS.sh
+++ Scripts/FlushDNS.sh
@@ -9,6 +9,7 @@ chmod +x /usr/local/bin/flushdns
 fi
 
 Version=$(defaults read /System/Library/CoreServices/SystemVersion ProductVersion | sed 's/\.//g')
+echo ""
 if [ $Version -gt 10104 ]; then
 sudo killall -HUP mDNSResponder
 echo "Flushed DNS Cache Successfully" 
@@ -20,11 +21,13 @@ sudo killall -HUP mDNSResponder
 echo "Flushed DNS Cache Successfully" 
 elif [ $Version -ge 10600 && $Version -lt 10700 ]; then
 sudo dscacheutil -flushcache
-echo "Flushed DNS Cache Successfully" 
+echo "Flushed DNS Cache Successfully"
+else
+echo "Unable to Flush DNS Cache"
 fi
 
 # Gives Suggestion to switch to Google Public DNS
 if [ $(whereis scutil) = /usr/sbin/scutil ]; then
 scutil --dns | grep -m 2 'nameserver\[[0-9]*\]'  | awk '{ print  $3 }' |  while read -r line ; do DNS=$(( $DNS + 1 )); if [ $DNS == 1 ]; then  echo "Primary DNS: " $line; elif [ $DNS == 2 ]; then echo "Secondary DNS: " $line; fi; done
-if  [ $(scutil --dns | grep -m 1 'nameserver\[[0-9]*\]'  | awk '{ print  $3 }') = 192.168.1.1 ]; then echo "Swiching to Google Public DNS will speed up your internet"; echo "for more information visit: j.mp/changemydns"; fi
+if  [ $(scutil --dns | grep -m 1 'nameserver\[[0-9]*\]'  | awk '{ print  $3 }') = 192.168.1.1 ]; then echo "Swiching to Google Public DNS will speed up your internet"; echo "for more information visit: j.mp/changemydns"; echo ""; fi
 fi
